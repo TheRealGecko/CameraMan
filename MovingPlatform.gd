342a1d5f6e3b4352fd25c67e2ec11f12807231e5
brrr <65472014+brrrb@users.noreply.github.com>
2022-08-06 18:37:24 -0400

    Add files via upload

---
+++ MovingPlatform.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+onready var platform = $Platform
+onready var tween = $MoveTween
+const IDLE_DURATION = 1.0 #num secs the platform stops before going back
+export var move_to = Vector2.UP * 128 
+export var speed = 3.0
+var follow = Vector2.ZERO
+
+func _ready():
+	_init_tween()
+	
+	#platform starts idling, moves to specified position, and starts idling moves back to initial position
+	
+func _init_tween():
+	var duration = move_to.length()/float(speed*32)
+	tween.interpolate_property(self, "follow", Vector2.ZERO, move_to, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT,IDLE_DURATION)
+	tween.interpolate_property(self, "follow",move_to,Vector2.ZERO, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT,duration+IDLE_DURATION*2)
+	tween.start( )
+	
+func _physics_process(delta):
+	platform.position = platform.position.linear_interpolate(follow, 0.075)
--- game.gd
+++ game.gd
@@ -3,27 +3,24 @@ extends Node2D
 onready var apoc_map = $Apocalyptic_TileMap
 onready var peace_map = $Peacful_TileMap
 onready var sprite = $player
+onready var moving_platform = $MovingPlatform
 
 func _ready():
 	apoc_map.visible = true
 	peace_map.visible = false
-	#sprite.set_collision_layer_bit(2, !true)
-	#sprite.set_collision_layer_bit(3, !false)
-	apoc_map.set_collision_layer(1)
-	peace_map.set_collision_layer(2)
+	sprite.set_collision_mask_bit(2, true)
+	sprite.set_collision_mask_bit(3, false)
 	
-var click_timer = Timer.new()
+#var click_timer = Timer.new()
 
-func _process(delta):
+"""func _process(delta):
 	if Input.is_action_just_pressed("toggle"):
 		click_timer = get_tree().create_timer(0.1)
 	if  Input.is_action_pressed("toggle") and click_timer != null and click_timer.get_time_left() <= 0:
 		print("works!")
-		#sprite.set_collision_layer_bit(2, !sprite.get_collision_layer_bit(2))
-		#sprite.set_collision_layer_bit(3, !sprite.get_collision_layer_bit(3))
-		#print(sprite.get_collision_layer_bit(3))
-		apoc_map.set_collision_layer(abs(apoc_map.get_collision_layer()-1))
-		peace_map.set_collision_layer(abs(peace_map.get_collision_layer()-1))
+		sprite.set_collision_mask_bit(2, !sprite.get_collision_mask_bit(2))
+		sprite.set_collision_mask_bit(3, !sprite.get_collision_mask_bit(3))
+		print(sprite.get_collision_mask_bit(3))
 		apoc_map.visible = !apoc_map.visible
 		peace_map.visible = !peace_map.visible
-		click_timer = null
+		click_timer = null"""
