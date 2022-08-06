3b7db70e18c1797aa025270d4de4b2c319dc7382
TheRealGecko <88910981+TheRealGecko@users.noreply.github.com>
2022-08-06 18:29:50 -0400

    TOGGLE WORKS AHHHHH

--- game.gd
+++ game.gd
@@ -7,8 +7,10 @@ onready var sprite = $player
 func _ready():
 	apoc_map.visible = true
 	peace_map.visible = false
-	sprite.set_collision_mask_bit(2, true)
-	sprite.set_collision_mask_bit(3, false)
+	#sprite.set_collision_layer_bit(2, !true)
+	#sprite.set_collision_layer_bit(3, !false)
+	apoc_map.set_collision_layer(1)
+	peace_map.set_collision_layer(2)
 	
 var click_timer = Timer.new()
 
@@ -17,9 +19,11 @@ func _process(delta):
 		click_timer = get_tree().create_timer(0.1)
 	if  Input.is_action_pressed("toggle") and click_timer != null and click_timer.get_time_left() <= 0:
 		print("works!")
-		sprite.set_collision_mask_bit(2, !sprite.get_collision_mask_bit(2))
-		sprite.set_collision_mask_bit(3, !sprite.get_collision_mask_bit(3))
-		print(sprite.get_collision_mask_bit(3))
+		#sprite.set_collision_layer_bit(2, !sprite.get_collision_layer_bit(2))
+		#sprite.set_collision_layer_bit(3, !sprite.get_collision_layer_bit(3))
+		#print(sprite.get_collision_layer_bit(3))
+		apoc_map.set_collision_layer(abs(apoc_map.get_collision_layer()-1))
+		peace_map.set_collision_layer(abs(peace_map.get_collision_layer()-1))
 		apoc_map.visible = !apoc_map.visible
 		peace_map.visible = !peace_map.visible
 		click_timer = null
